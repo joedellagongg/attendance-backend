43fbe1952125301c1aa78bf26599d76e9ebd753e
Joedel Lagong <joedelsalaslagong@gmail.com>
2024-10-14 02:12:13 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,48 +1,50 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
 
-async function databaseInitiator() {
-    try {
-        const mydb = mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE || "dbsts",
-            port: process.env.PORT || 15662,
-        });
-        console.log("\x1b[32m%s\x1b[0m", "  ▲", "[ DATABASE ] Connected!");
-        return mydb;
-    } catch (error) {
-        console.log("\n🚧 Error: Not connected to MySQL Database:");
-        console.log("   | - If it's not Connected ");
-        console.log("   | - Check if MySQL and APACHE is turned off.\n ");
-        console.log(error);
-    }
-}
-
-module.exports = databaseInitiator();
-
-// const mydb = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE || "dbsts",
-//     port: process.env.PORT || 15662,
-// });
-
-// mydb.connect((err) => {
-//     if (err) {
-//         console.log(process.env.DB_HOST);
-//         console.log(process.env.DB_USER);
-//         console.log(process.env.DB_PASSWORD);
-//         console.log(process.env.DB_DATABASE);
-//         console.log(process.env.DB_PORT);
+// async function databaseInitiator() {
+//     try {
+//         const mydb = mysql.createConnection({
+//             host: process.env.DB_HOST,
+//             user: process.env.DB_USER,
+//             password: process.env.DB_PASSWORD,
+//             database: process.env.DB_DATABASE || "dbsts",
+//             port: process.env.PORT || 15662,
+//         });
+//         console.log("\x1b[32m%s\x1b[0m", "  ▲", "[ DATABASE ] Connected!");
+//         return mydb;
+//     } catch (error) {
 //         console.log("\n🚧 Error: Not connected to MySQL Database:");
 //         console.log("   | - If it's not Connected ");
 //         console.log("   | - Check if MySQL and APACHE is turned off.\n ");
-//         console.log(err);
-//     } else {
-//         console.log("\x1b[32m%s\x1b[0m", "  ▲", "[ DATABASE ] Connected!");
+//         console.log(error);
 //     }
-// });
+// }
+
+// module.exports = databaseInitiator();
+
+const mydb = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE || "dbsts",
+    port: process.env.PORT || 15662,
+});
+
+mydb.connect((err) => {
+    if (err) {
+        console.log(process.env.DB_HOST);
+        console.log(process.env.DB_USER);
+        console.log(process.env.DB_PASSWORD);
+        console.log(process.env.DB_DATABASE);
+        console.log(process.env.DB_PORT);
+        console.log("\n🚧 Error: Not connected to MySQL Database:");
+        console.log("   | - If it's not Connected ");
+        console.log("   | - Check if MySQL and APACHE is turned off.\n ");
+        console.log(err);
+    } else {
+        console.log("\x1b[32m%s\x1b[0m", "  ▲", "[ DATABASE ] Connected!");
+    }
+});
+
+module.exports = mydb;
