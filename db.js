3d45858c1cbdd426d380adbbaca8d0a6326ea765
Joedel Lagong <joedelsalaslagong@gmail.com>
2024-10-13 16:52:45 +0800

    Update db.js

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const mydb = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE || "dbsts",
-    port: process.env.PORT,
+    port: process.env.PORT || 15662,
 });
 
 // const mydb = mysql.createConnection({
